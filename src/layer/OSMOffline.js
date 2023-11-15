2d611617ff87f3455f13f6ea2c8019970e024509
Jonas Söderström <jonas.soderstrom@xlent.se>
2023-11-15 14:34:40 +0100

    Revert some tried to fix linting

--- src/layer/OSMOffline.js
+++ src/layer/OSMOffline.js
@@ -1,15 +1,14 @@
-/* eslint linebreak-style: ["error", "windows"] */
-import OSMSource from 'OSMOfflineSource';
+import OSMSource from './OSMOfflineSource';
 import tile from './tile';
 
 export default function osmoffline(layerOptions) {
   const osmDefault = {};
   const osmOptions = Object.assign(osmDefault, layerOptions);
 
-  function createSource() {
-    return new OSMSource();
-  }
+    function createSource() {
+        return new OSMSource();
+    }
 
-  const osmSource = createSource();
-  return tile(osmOptions, osmSource);
+    const osmSource = createSource();
+    return tile(osmOptions, osmSource);
 }
