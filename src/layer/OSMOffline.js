967e79839ae78fd037e3123278fe080f848775cb
Jonas Söderström <jonas.soderstrom@xlent.se>
2023-11-15 14:22:39 +0100

    Try to fix linting

--- src/layer/OSMOffline.js
+++ src/layer/OSMOffline.js
@@ -1,14 +1,15 @@
-import OSMSource from './OSMOfflineSource';
+/* eslint linebreak-style: ["error", "windows"] */
+import OSMSource from 'OSMOfflineSource';
 import tile from './tile';
 
 export default function osmoffline(layerOptions) {
-    const osmDefault = {};
-    const osmOptions = Object.assign(osmDefault, layerOptions);
+  const osmDefault = {};
+  const osmOptions = Object.assign(osmDefault, layerOptions);
 
-    function createSource() {
-        return new OSMSource();
-    }
+  function createSource() {
+    return new OSMSource();
+  }
 
-    const osmSource = createSource();
-    return tile(osmOptions, osmSource);
+  const osmSource = createSource();
+  return tile(osmOptions, osmSource);
 }
